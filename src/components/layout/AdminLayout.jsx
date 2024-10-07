8931852a5f8f79bdb88bb1e8757661f032b716d7
CamilleSilene <trillatcam@gmail.com>
2024-10-07 20:06:11 +0200

    ajout navigation dans admin layout

--- src/components/layout/AdminLayout.jsx
+++ src/components/layout/AdminLayout.jsx
@@ -1,6 +1,6 @@
 import Container from "react-bootstrap/esm/Container";
 import { Outlet } from "react-router-dom";
-import AdminNavigation from "../project/admin/AdminNavigation.jsx/AdminNavigation";
+import AdminNavigation from "../project/admin/AdminNavigation/AdminNavigation";
 
 const AdminLayout = () => {
   return (
