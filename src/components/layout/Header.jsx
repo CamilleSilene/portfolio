5e3398a069d7a38d2c9d7f923c73269b0cf49c57
CamilleSilene <trillatcam@gmail.com>
2024-10-11 21:21:51 +0200

    start animation

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,9 +2,10 @@ import Navigation from "./Navigation";
 
 function Header() {
     return (
-    <div className="container-fluid bg-secondary">
+    <div className="container-fluid bg-transparent bgstyle">
       <div className='row'>
         <Navigation />
+        <div id="stars"></div>
         <div clas="col-12" style={{ height: '20vh'}}><h1>Header</h1></div>
       </div>
     </div>
