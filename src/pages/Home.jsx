a81628e6844de987f15fba7beb3d309df3e5a799
CamilleSilene <trillatcam@gmail.com>
2024-10-19 20:25:26 +0200

    retour vers stars en css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,26 +4,11 @@ import Project from "../components/project/Projects";
 import Header from "../components/layout/Header";
 function Home( ) {
 
-    
-    const [stars, setStars] = useState([]);
-    useEffect(() => {
-        function buildStars( ) {
-            let stars = []
-            for( let i=0; i < 2000 ; i++ ) {
-                stars[i] = `${Math.random()*3000}px ${Math.random()*2000}px #fff`
-            }
-            return stars;
-        }
-        setStars(buildStars());
-      }, []);
-
-
-
     return (
         <>
             <Header />
             <AboutMe />
-            <div id="stars" style={{'boxShadow': stars.join(','), ':after': stars.join(',')}}></div>
+            <div id="stars"></div>
             <Project />
         </>
     );
